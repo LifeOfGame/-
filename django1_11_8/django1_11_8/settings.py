51fa333d9544b6fa692815806dc20bbda00887d1
hlf <lfltbrothers@yeah.net>
2019-07-12 12:49:31 +0800

    blog

--- django1_11_8/django1_11_8/settings.py
+++ django1_11_8/django1_11_8/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'student',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
